83e2d6d34259afafea0e4b459771fc39f2819843
Haddib <hb150794@gmail.com>
2019-02-22 13:38:54 +0000

    Elevator og Person klasar

---
+++ src/com/ru/usty/elevator/Elevator.java
@@ -0,0 +1,8 @@
+package com.ru.usty.elevator;
+
+public class Elevator implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/com/ru/usty/elevator/Person.java
@@ -0,0 +1,8 @@
+package com.ru.usty.elevator;
+
+public class Person implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
